3f6a7ef6db89e4057047f2947d0b40a276f212c1
sakasegawayosuke <60595083+sakasegawayosuke@users.noreply.github.com>
2020-11-02 10:20:46 +0900

    Update generator.py

--- generator.py
+++ generator.py
@@ -7,7 +7,9 @@ from dataloader import dataset_for_generator
 class Generator(RNNLM):
     def __init__(self, num_emb, batch_size, emb_dim, hidden_dim, sequence_length, start_token, learning_rate=0.01):
         super(Generator, self).__init__(num_emb, batch_size, emb_dim, hidden_dim, sequence_length, start_token, learning_rate)
+        
         # prepare model for GAN training
+        # GANのモデルを作成
         self.g_model_temporal = tf.keras.models.Sequential(self.g_model.layers)
         self.g_optimizer_temporal = self._create_optimizer(
             learning_rate, clipnorm=self.grad_clip)
@@ -16,9 +18,12 @@ class Generator(RNNLM):
             loss="sparse_categorical_crossentropy",
             sample_weight_mode="temporal")
 
+# 事前学習用のモデル
     def pretrain(self, dataset, target_lstm, num_epochs, num_steps, eval_file):
         # dataset: each element has [self.batch_size, self.sequence_length]
         # outputs are 1 timestep ahead
+        
+        # 途中経過を表示
         def pretrain_callback(epoch, logs):
             if epoch % 5 == 0:
                 self.generate_samples(num_steps, eval_file)
@@ -29,10 +34,12 @@ class Generator(RNNLM):
                 # log.write(buffer)
 
         ds = dataset.map(lambda x: (tf.pad(x[:, 0:-1], ([0, 0], [1, 0]), "CONSTANT", self.start_token), x)).repeat(num_epochs)
+        # 事前学習の実行
         pretrain_loss = self.g_model.fit(ds, verbose=1, epochs=num_epochs, steps_per_epoch=num_steps,
                                          callbacks=[tf.keras.callbacks.LambdaCallback(on_epoch_end=pretrain_callback)])
         return pretrain_loss
 
+ # GANの生成器のモデル
     def train_step(self, x, rewards):
         # x: [self.batch_size, self.sequence_length]
         # rewards: [self.batch_size, self.sequence_length] (sample_weight)
