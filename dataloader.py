0b23aa87223ef7d52b91f751d29c838f16c0a74c
sakasegawayosuke <60595083+sakasegawayosuke@users.noreply.github.com>
2020-11-09 22:00:36 +0900

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -8,12 +8,10 @@ def dataset_for_generator(data_file, batch_size):
             line = line.split()
             parse_line = [int(x) for x in line]
             
-            # 今回は文字列の長さは指定しないので
-#             if len(parse_line) == 20:
-            token_stream.append(parse_line)
+            if len(parse_line) == 20:
+#             token_stream.append(parse_line)
     
-    data = tf.ragged.constant(token_stream)
-    return tf.data.Dataset.from_tensor_slices(data).shuffle(len(token_stream)).batch(batch_size)
+    return tf.data.Dataset.from_tensor_slices(token_stream).shuffle(len(token_stream)).batch(batch_size)
 
 def dataset_for_discriminator(positive_file, negative_file, batch_size):
     examples = []
@@ -23,16 +21,16 @@ def dataset_for_discriminator(positive_file, negative_file, batch_size):
             line = line.strip()
             line = line.split()
             parse_line = [int(x) for x in line]
-#             if len(parse_line) == 20:
-            examples.append(parse_line)
-            labels.append([0, 1])
-        
+            if len(parse_line) == 20:
+                examples.append(parse_line)
+                labels.append([0, 1])
+
     with open(negative_file) as fin:
         for line in fin:
             line = line.strip()
             line = line.split()
             parse_line = [int(x) for x in line]
-#             if len(parse_line) == 20:
-            examples.append(parse_line)
-            labels.append([1, 0])
+            if len(parse_line) == 20:
+                examples.append(parse_line)
+                labels.append([1, 0])
     return tf.data.Dataset.from_tensor_slices((examples, labels)).shuffle(len(examples)).batch(batch_size)
