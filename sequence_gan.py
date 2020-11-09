e80776b1f311f9b3e994808650bca1727a81cd43
sakasegawayosuke <60595083+sakasegawayosuke@users.noreply.github.com>
2020-11-09 21:05:07 +0900

    Update sequence_gan.py

--- sequence_gan.py
+++ sequence_gan.py
@@ -44,7 +44,7 @@ TOTAL_BATCH = 200 # バッチサイズ
 
 # 学習で使用するデータ
 # 最初は存在しないので、lstmで作るらしい
-positive_file = 'save/real_data.txt'
+positive_file = 'save/hakataeki_haiku2id.txt'
 negative_file = 'save/generator_sample.txt'
 # このファイルはどうした？
 eval_file = 'save/eval_file.txt'
@@ -65,13 +65,13 @@ def main():
         for dev in physical_devices:
             tf.config.experimental.set_memory_growth(dev, True)
 
-    # 生成器で学習
+    # 生成器の初期設定
     generator = Generator(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, START_TOKEN)
     # 最初のパラメータをpicklファイルから参照
     target_params = pickle.load(open('save/target_params_py3.pkl', 'rb'))
     target_lstm = TARGET_LSTM(BATCH_SIZE, SEQ_LENGTH, START_TOKEN, target_params) # The oracle model
 
-    # 識別器で学習
+    # 識別器の初期設定
     discriminator = Discriminator(sequence_length=SEQ_LENGTH, num_classes=2, vocab_size=vocab_size, embedding_size=dis_embedding_dim,
                                   filter_sizes=dis_filter_sizes, num_filters=dis_num_filters, dropout_keep_prob=dis_dropout_keep_prob,
                                   l2_reg_lambda=dis_l2_reg_lambda)
